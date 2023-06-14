36b81275a10f6d231a1c75042e0090cb7bbbfa60
Deep  soni <fichadiyadeep@gmail.com>
2023-06-14 20:01:54 +0000

    fix

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(function (req, res, next) {
 
     res.header('Access-Control-Allow-Origin', "https://whatsapp-sv52.onrender.com");
     res.header('Access-Control-Allow-Headers', true);
-    res.header('Access-Control-Allow-Credentials', 'Content-Type');
+    res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     next();
   });
